ef6b1bc0b53634d7d81da9a546b36d82774fc68b
Aiden Dickens <aiden.dickens@43540.auckland.natcoll.ac.nz>
2014-06-25 17:30:25 +1200

    first commit

---
+++ Stage_5_site/includes/synopsis.php
@@ -0,0 +1,22 @@
+     <div class="synopsis">
+                <h1>synopsis</h1>
+                <p>Release date:    16/06/2012<br><br>
+                    Rating:     M<br><br>   
+                    Genre:      Action/Horror<br><br>
+                    Running time:   116 mins<br><br>
+
+                    Director:       Aidan Dickens<br><br>
+                    Cast:           Lee Ah Yen, Alex Arruda, Rebecca Parr<br><br>
+
+                    <p>Trapped together in a basement after 
+                        an environmental catastrophe,
+                        Frank and Donald await their fate.
+                        But what secrets is Frank concealing 
+                        and who is the mysterious stranger
+                        who keeps calling him? Donald 
+                        wants to leave and becomes more and 
+                        more agitated and Frank says he 
+                        can't because it's not safe out side yet.
+                        As the tension rises it becomes clear
+                        that somethings gonna give, and soon...</p> 
+                    </div>
